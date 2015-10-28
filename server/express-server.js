013008a89fb6e5f3ae037acffb5ffb99ec6740a8
nickjakubowski <ncjakubowski@gmail.com>
2015-10-27 18:44:38 -0700

    Implemented express server

---
+++ server/express-server.js
@@ -0,0 +1,66 @@
+/* Import node's http module: */
+var http = require("http");
+var messages = require("./request-handler");
+// var url = require('url');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var port = 3000;
+var ip = "127.0.0.1";
+
+var storage = [];
+
+var app = express();
+var server = http.createServer(app);
+
+app.use('/', express.static(__dirname + '/../client'));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/../client/index.html')
+})
+
+app.use(function(req, res, next) {
+  res.header(headers);
+  if (req.method === 'OPTIONS') {
+    res.header('Content-Type', 'text/plain');
+    res.status(200).send();
+  } else {
+    next();
+  }
+});
+
+
+app.get('/classes/messages', function(req, res) {
+  res.header('Content-type', 'application/json');
+  res.status(200).send('{"results" :' + JSON.stringify(storage) + '}');
+});
+
+app.use(bodyParser.json());
+
+app.post('/classes/messages', function(req, res) {
+  storage.push(req.body);
+  res.status(201).send("OK")
+});
+
+
+console.log("Listening on http://" + ip + ":" + port);
+server.listen(port, ip);
+
+// To start this server, run:
+//
+//   node basic-server.js
+//
+// on the command line.
+//
+// To connect to the server, load http://127.0.0.1:3000 in your web
+// browser.
+//
+// server.listen() will continue running as long as there is the
+// possibility of serving more requests. To stop your server, hit
+// Ctrl-C on the command line.
+
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
